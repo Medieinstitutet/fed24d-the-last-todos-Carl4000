5ca77384e7a641cde1a6cb6e37245e2e0bfcac76
Carl4000 <cmwellman@gmail.com>
2025-06-04 20:44:18 +0200

    overall all updates. mainly local storage

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,59 +1,66 @@
-import { useState, type ChangeEvent } from "react"
-import { Task } from "../models/Task"
+import { useState, type ChangeEvent } from 'react';
+import { Task } from '../models/Task';
 
 export const TodoList = () => {
-    const [tasks, setTasks] = useState<Task[]>(() => { 
-      
-      const localData = localStorage.getItem("tasks");
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const localData = localStorage.getItem('tasks');
 
-      if (localData) {
-        const localDataParsed = JSON.parse(localData)
-        return localDataParsed.map((t: Task) => new Task(t.id, t.task, t.done));
-      } else  {
-        return [
-        new Task(1, "Koda", false),
-        new Task(2, "Felsöka", false),
-        new Task(3, "Tvätta bilen", false),
-        ];
-      }
-    });
+    if (localData) {
+      const localDataParsed = JSON.parse(localData);
+      return localDataParsed.map((t: Task) => new Task(t.id, t.task, t.done));
+    } else {
+      return [
+        new Task(1, 'Koda', false),
+        new Task(2, 'Felsöka', false),
+        new Task(3, 'Tvätta bilen', false),
+      ];
+    }
+  });
 
-    // Ta bort ur listan
-    const removeTask = (id: number) => {
-        setTasks(tasks.filter((t) => t.id !== id));
-    };
-
-    const updateTask = (id: number, done: boolean) => {
-    setTasks(tasks.map((t) => {
-        if (t.id === id) {
-          return { ...t, done: done };
-        }
-        return t;
-      })
-    );
+  // Ta bort ur listan
+  const removeTask = (id: number) => {
+    const taskList = tasks.filter((t) => t.id !== id);
+    setTasks(taskList);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
   };
 
-   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.type === "checkbox") {
-      const id = +e.target.id; 
+  const updateTask = (id: number, done: boolean) => {
+    const taskList = tasks.map((t) => {
+      if (t.id === id) {
+        return { ...t, done: done };
+      }
+      return t;
+    });
+    setTasks(taskList);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.type === 'checkbox') {
+      const id = +e.target.id;
+
       updateTask(id, e.target.checked);
     }
   };
-    
 
-    return <>
-        <ul>
-            
-            {tasks.map((t) => <li key={t.id}>
-                <h4>{t.task}</h4>
-                Done: <input type="checkbox" checked={t.done} onChange={handleChange} id={String(t.id)} />
-                <br></br>
-                <button onClick={() => removeTask(t.id)} >Ta bort</button>
-            </li>)}
-
-        
-        </ul>
-    
+  return (
+    <>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t.id}>
+            <h4>{t.task}</h4>
+            Done:{' '}
+            <input
+              type="checkbox"
+              checked={t.done}
+              onChange={handleChange}
+              id={String(t.id)}
+            />
+            <br></br>
+            <button onClick={() => removeTask(t.id)}>Ta bort</button>
+          </li>
+        ))}
+      </ul>
     </>
-
+  );
 };
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,11 +1,11 @@
 export class Task {
-    public id: number;
-    public task: string;
-    public done: boolean;
+  public id: number;
+  public task: string;
+  public done: boolean;
 
-    constructor(id: number, task: string, done: boolean) {
-        this.id = id;
-        this.task = task;
-        this.done = done;
-    }
+  constructor(id: number, task: string, done: boolean) {
+    this.id = id;
+    this.task = task;
+    this.done = done;
+  }
 }
